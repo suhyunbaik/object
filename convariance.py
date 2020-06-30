c4cf7df6acd83fd1340b293cbcdf6c937aebf7ab
suhyunbaik <suhyunbaik@ogqcorp.com>
2020-06-30 15:42:11 +0900

    Feat: add convariance example

---
+++ convariance.py
@@ -0,0 +1,42 @@
+class Publisher(object):
+    def __init__(self):
+        pass
+
+
+class IndependentPublisher(Publisher):
+    def __init__(self):
+        super().__init__()
+
+
+class Book(object):
+    def __init__(self, publisher=None):
+        self.__publisher = publisher
+
+
+class Magazine(Book):
+    def __init__(self, publisher):
+        super().__init__(publisher)
+
+
+class BookStall(object):
+    def sell(self, independent_publisher: IndependentPublisher) -> Book:
+        return Book(independent_publisher)
+
+
+class MagazineStore(BookStall):
+    def sell(self, independent_publisher: IndependentPublisher) -> Book:
+        return Magazine(independent_publisher)
+
+
+class Customer(object):
+    def __init__(self, book=None):
+        self.__book: Book = book
+
+    def order(self, bookstall: BookStall):
+        self.__book = bookstall.sell(IndependentPublisher())
+
+
+if __name__ =='__main__':
+    customer = Customer().order(BookStall())
+    customers = Customer().order(MagazineStore())
+    print(customer, customers)
